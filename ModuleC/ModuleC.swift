03c039672368f14bc1a5f947aed6678681539d3c
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-03-01 03:09:58 +0700

    initial

---
+++ ModuleC/ModuleC.swift
@@ -0,0 +1,16 @@
+//
+//  ModuleC.swift
+//  ModuleC
+//
+//  Created by Auzan Lazuardi on 01/03/24.
+//
+
+import Foundation
+
+public class ModuleC {
+    public static let shared = ModuleC()
+    
+    public func getData() {
+        print("Modul C here")
+    }
+}
