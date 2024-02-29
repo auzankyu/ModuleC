f47d0b7632675d45e2bd5eb62b62d3d5eb1c7f73
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-03-01 02:28:50 +0700

    Initial Commit

---
+++ ModuleC/ModuleC.h
@@ -0,0 +1,18 @@
+//
+//  ModuleC.h
+//  ModuleC
+//
+//  Created by Auzan Lazuardi on 01/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleC.
+FOUNDATION_EXPORT double ModuleCVersionNumber;
+
+//! Project version string for ModuleC.
+FOUNDATION_EXPORT const unsigned char ModuleCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleC/PublicHeader.h>
+
+
